c20755d9147ad0bf1338fa880f25bba1bbd87103
conrad <conrad@pythonanywhere.com>
2018-10-30 17:40:12 +0000

    separate out square vs piece concept and first cut at making the repr display for a board

--- board.py
+++ board.py
@@ -1,22 +1,29 @@
+import string
+from itertools import cycle
+from typing import List, NamedTuple, Optional
+
+from colorama import Back, Fore, Style
+from tabulate import tabulate
+
 from dataclasses import dataclass
-from typing import List, NamedTuple, Union
 
 # We make the type of Colour just a boolean for now so that we can flexibly change this later
-Colour = bool
+Color = bool
 
 
-class Colours(NamedTuple):
-    white: Colour = False
-    black: Colour = True
+class Colors(NamedTuple):
+    white: Color = False
+    black: Color = True
 
 
-COLOUR = Colours()
+COLOR = Colors()
 
 
+# dataclass has auto init/repr etc, and also frozen option raises exception if fields are assigned to
 @dataclass(frozen=True)
 class Piece:
     name: str
-    color: Colour
+    color: Color
     abbreviation: str
     symbol: str
 
@@ -24,6 +31,41 @@ class Piece:
         return self.symbol
 
 
+@dataclass(frozen=True)
+class Square:
+    coordinate: str
+    color: Color
+    symbol: str
+    piece: Optional[Piece]
+
+    def __repr__(self) -> str:
+        return self.symbol
+
+
 class Board:
-    mailbox: List[List[Union[Piece, None]]] = [[None] * 8] * 8
-    next_move: Colour = COLOUR.white
+    # internal representation of the board is "mailbox" format
+    # ie. a square-centric board representation where the encoding of every
+    # square resides in a separately addressable memory element The square
+    # number, or its file and rank, acts like an address to a post box, which
+    # might be empty or may contain one chess piece.
+    mailbox: List[List[Square]]
+    next_move: Color = COLOR.white
+
+    def __init__(self) -> None:
+        self.mailbox = []
+        color_iterator = cycle([COLOR.black, COLOR.white])
+        # symbol_iterator = cycle("\u25A0\u25A1")
+        for row_number in range(1, 9):
+            row = []
+            for col_alpha in string.ascii_lowercase[:8]:
+                row.append(
+                    Square(coordinate=f"{col_alpha}{row_number}", color=next(color_iterator), symbol=" ", piece=None)
+                )
+            self.mailbox.append(row)
+
+    def __repr__(self) -> str:
+        # clear screen
+        # board_string = "\x1b[2J"
+        board_string = Fore.BLACK + Back.WHITE + "\n"
+        board_string += tabulate(reversed(self.mailbox), tablefmt="fancy_grid")
+        return board_string + Style.RESET_ALL
--- test_board.py
+++ test_board.py
@@ -1,18 +1,36 @@
-from board import COLOUR, Board, Piece
+import pytest
+
+from board import COLOR, Board, Piece
 
 
 def test_pieces_displayed_as_unicode_symbol() -> None:
-    king = Piece(name="King", color=COLOUR.black, abbreviation="K", symbol=u"\u2654")
-    assert str(king) == "\u2654"
+    king = Piece(name="King", color=COLOR.black, abbreviation="K", symbol=u"\u2654")
+    assert str(king) == "♔"
 
 
-def test_board_creation_defaults_to_empty_board() -> None:
-    new_board = Board()
+@pytest.fixture
+def new_board() -> Board:
+    return Board()
+
+
+def test_board_creation_defaults_to_empty_board(new_board: Board) -> None:
     for row in new_board.mailbox:
         for square in row:
-            assert square is None
+            assert square.piece is None
 
 
-def test_board_creation_defaults_to_white_to_move() -> None:
-    new_board = Board()
-    assert new_board.next_move == COLOUR.white
+def test_board_creation_defaults_to_white_to_move(new_board: Board) -> None:
+    assert new_board.next_move == COLOR.white
+
+
+def test_board_created_with_correct_algebraic_notation_coordinates(new_board: Board) -> None:
+    assert new_board.mailbox[0][0].coordinate == "a1"
+
+
+def test_board_created_with_colored_squares(new_board: Board) -> None:
+    assert new_board.mailbox[0][0].color == COLOR.black
+
+
+def test_can_print_board(new_board: Board) -> None:
+    expected_empty_board = "\x1b[30m\x1b[47m\n╒══╤══╤══╤══╤══╤══╤══╤══╕\n│  │  │  │  │  │  │  │  │\n├──┼──┼──┼──┼──┼──┼──┼──┤\n│  │  │  │  │  │  │  │  │\n├──┼──┼──┼──┼──┼──┼──┼──┤\n│  │  │  │  │  │  │  │  │\n├──┼──┼──┼──┼──┼──┼──┼──┤\n│  │  │  │  │  │  │  │  │\n├──┼──┼──┼──┼──┼──┼──┼──┤\n│  │  │  │  │  │  │  │  │\n├──┼──┼──┼──┼──┼──┼──┼──┤\n│  │  │  │  │  │  │  │  │\n├──┼──┼──┼──┼──┼──┼──┼──┤\n│  │  │  │  │  │  │  │  │\n├──┼──┼──┼──┼──┼──┼──┼──┤\n│  │  │  │  │  │  │  │  │\n╘══╧══╧══╧══╧══╧══╧══╧══╛\x1b[0m"  # noqa
+    assert str(new_board) == expected_empty_board
