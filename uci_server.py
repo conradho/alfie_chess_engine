ee01227bcdc432f2d3e052debe13d5a426951925
conrad <conrad@pythonanywhere.com>
2018-11-09 18:07:47 +0000

    add async code to uci server to catch sigint and exit gracefully

--- uci_server.py
+++ uci_server.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import logging
+import signal
 import sys
 from pathlib import Path
 
@@ -10,7 +11,24 @@ logging.basicConfig(filename=str(Path("./chess_engine.log").resolve()), level=lo
 async def heartbeat() -> None:
     while True:
         logging.debug(datetime.datetime.now())
-        await asyncio.sleep(5)
+        await asyncio.sleep(600)
+
+
+async def stop_all_tasks() -> None:
+    tasks = [t for t in asyncio.Task.all_tasks() if t is not asyncio.tasks.Task.current_task()]
+    for task in tasks:
+        task.cancel()
+    await asyncio.wait(tasks, timeout=0.5)
+    asyncio.get_event_loop().stop()
+    logging.debug("stopped")
+
+
+def ask_exit() -> None:
+    # this is just a synchronous function that puts something onto the loop. we
+    # need to cancel the tasks from within the loop because after we cancel, we
+    # want to wait for the tasks to complete before stopping the loop
+    logging.debug("stopping")
+    asyncio.get_event_loop().create_task(stop_all_tasks())
 
 
 def process_stdin() -> None:
@@ -18,10 +36,16 @@ def process_stdin() -> None:
     logging.debug(f"received from stdin: {repr(stdin)}")
 
 
-if __name__ == "__main__":
+def run_server() -> None:
     loop = asyncio.get_event_loop()
     # calling the hearbeat function returns a coroutine and create_task takes a
     # coro and turns it into a task to be executed in the loop
     loop.create_task(heartbeat())
     loop.add_reader(sys.stdin.fileno(), process_stdin)
+    loop.add_signal_handler(signal.SIGINT, ask_exit)
     loop.run_forever()
+    loop.close()
+
+
+if __name__ == "__main__":
+    run_server()
